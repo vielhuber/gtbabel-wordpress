3ad4508b4a0e39d5833ae01c47ff78405339c6b3
David Vielhuber <david@vielhuber.de>
2021-05-02 02:11:33 +0200

    Next major release

--- gtbabel.php
+++ gtbabel.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gtbabel
  * Plugin URI: https://www.gtbabel.com
  * Description: Instant server-side translation of any page.
- * Version: 5.8.2
+ * Version: 6.0.0
  * Author: Gtbabel
  * Author URI: https://www.gtbabel.com
  * License: free
