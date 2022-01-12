387f3bd86f559903276894baeb749f69049506d8
David Vielhuber <david@vielhuber.de>
2022-01-12 02:12:13 +0100

    Improve deploy test debug script.

--- deploy/gtbabel-dev.sh
+++ deploy/gtbabel-dev.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
+BASE_PLUGIN="/var/www/gtbabel"
+BASE_WORDPRESS="/var/www/close2"
+
+FOLDER=false
+if [ -d "$BASE_WORDPRESS/wp-content/plugins/gtbabel" ]; then
+    FOLDER="gtbabel_tmp"
+fi
+if [ -d "$BASE_WORDPRESS/wp-content/plugins/gtbabelpro" ]; then
+    FOLDER="gtbabelpro"
+fi
+if [[ "$FOLDER" == false ]]; then
+    echo "folder missing"
+    exit
+fi
+
 if [[ ( -n "$1" ) && ( $1 == "start" ) ]]; then
-    if [ ! -d "./../../plugins/gtbabel_tmp" ]; then
+    if [ ! -d "$BASE_WORDPRESS/wp-content/plugins/gtbabel_tmp" ]; then
         echo "starting..."
-        mv ./../../plugins/gtbabel ./../../plugins/gtbabel_tmp
-        ln -s /var/www/gtbabel/wordpress ./../../plugins/gtbabel
+        mv "$BASE_WORDPRESS/wp-content/plugins/$FOLDER" "$BASE_WORDPRESS/wp-content/plugins/gtbabel_tmp"
+
+        if [[ "$FOLDER" == "gtbabelpro" ]]; then
+            sed -i -e "s/private \$name = 'Gtbabel'/private \$name = 'Gtbabel Pro'/g" "$BASE_PLUGIN/wordpress/gtbabel.php"
+            mv "$BASE_PLUGIN/wordpress/gtbabel.php" "$BASE_PLUGIN/wordpress/gtbabelpro.php"
+        fi
+
+        ln -s "$BASE_PLUGIN/wordpress" "$BASE_WORDPRESS/wp-content/plugins/$FOLDER"
     else
         echo "stop first!"
     fi
 fi
 
 if [[ ( -n "$1" ) && ( $1 == "stop" ) ]]; then
-    if [ -d "./../../plugins/gtbabel_tmp" ]; then
+    if [ -d "$BASE_WORDPRESS/wp-content/plugins/gtbabel_tmp" ]; then
         echo "stopping..."
-        unlink ./../../plugins/gtbabel
-        mv ./../../plugins/gtbabel_tmp ./../../plugins/gtbabel
+        unlink "$BASE_WORDPRESS/wp-content/plugins/$FOLDER"
+
+        if [[ "$FOLDER" == "gtbabelpro" ]]; then
+            sed -i -e "s/private \$name = 'Gtbabel Pro'/private \$name = 'Gtbabel'/g" "$BASE_PLUGIN/wordpress/gtbabelpro.php"
+            mv "$BASE_PLUGIN/wordpress/gtbabelpro.php" "$BASE_PLUGIN/wordpress/gtbabel.php"
+        fi
+
+        mv "$BASE_WORDPRESS/wp-content/plugins/gtbabel_tmp" "$BASE_WORDPRESS/wp-content/plugins/$FOLDER"
     else
         echo "start first!"
     fi
