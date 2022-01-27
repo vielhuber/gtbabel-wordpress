1b84b56839665eb346d47408bf06fc545c392f61
David Vielhuber <david@vielhuber.de>
2022-01-27 23:31:08 +0100

    6.4.3

--- gtbabel.php
+++ gtbabel.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gtbabel
  * Plugin URI: https://www.gtbabel.com
  * Description: Instant server-side translation of any page.
- * Version: 6.4.1
+ * Version: 6.4.3
  * Author: Gtbabel
  * Author URI: https://www.gtbabel.com
  * License: free
